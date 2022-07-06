09199dd04fbfbfc39b2bc0fe6c2cedcbfb7bca1d
Lukinhas123Ito <lucastoshihiroito28@gmail.com>
2022-07-06 15:32:59 -0300

    alter here

--- conn/conn.php
+++ conn/conn.php
@@ -5,3 +5,13 @@ $dbname = "adsolucoestecn15";
 $username = "adsolucoestecn15";
 $password = "TR31NAM3NT0";
 
+try{
+    $pdo = new PDO('mysql:host='.$hostname.';dbname='.$dbname, $username, $password);
+    $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "<img src="."ae.png".">";
+}catch(PDOException $e){
+
+    echo "<img src="."Gru.png".">\n";
+    echo "Volta lá no pois o erro está no: ".$e->getMessage();
+}
+
