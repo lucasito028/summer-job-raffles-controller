5a870ac1e64fa215bf167677bace850e411416da
Lukinhas123Ito <lucastoshihiroito28@gmail.com>
2022-07-06 10:33:43 -0300

    some alter

---
+++ src/cliente/controll/add.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.btn-new').click(function(e){
+
+        e.preventDefault()
+
+        $('.modal-title').empty()
+        $('.modal-body').empty()
+
+        $('.modal-title').append('Adiciona algo ai')
+
+        $('.modal-body').load('src/cliente/view/form-cliente.html')
+
+        $('#modal-cliente').modal('show')
+    })
+})
