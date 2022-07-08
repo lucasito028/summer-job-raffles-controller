c125006257d1233d752bb310a471e44b16f4ac6f
Lukinhas123Ito <lucastoshihiroito28@gmail.com>
2022-07-08 13:02:26 -0300

    correting some mistake

--- src/empresa/controll/update.js
+++ src/empresa/controll/update.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
-    $('#table-emprese').on('click', 'button.btn-edit', function(){
+    $('#table-empresa').on('click', 'button.btn-edit', function(e){
+
+        e.preventDefault()
 
         $('.modal-title').empty()
         $('.modal-body').empty()
@@ -9,12 +11,32 @@ $(document).ready(function(){
         let id = `ID=${$(this).attr('id')}`
 
         $.ajax({
-            url:'src/empresa/model/update.php',
+            url:'src/empresa/model/list-empresa.php',
             data:id,
             dataType:'json',
             assync: true,
             type: 'POST',
             success:function(dados){
+                if(dados.tipo == 'success'){
+                    $('.modal-body').load('src/empresa/view/form-empresa.html', function(){
+                        $('#NOME').val(dados.dados.NOME)
+                        $('#LOGIN').val(dados.dados.LOGIN)
+                        $('#SENHA').val(dados.dados.SENHA)
+                        $('#ID').val(dados.dados.ID)
+                        
+                    })
+                    $('.btn-save').show()
+                    $('.btn-save').removeAttr('data-operation')
+                    $('#modal-empresa').modal('show')
+
+                }else{
+                    Swal.fire({
+                        title: 'Sistema',
+                        text: dados.message,
+                        icon: dados.tipo,
+                        confirmButtonText: 'OK'
+                    })
+                }
 
             }
         })
